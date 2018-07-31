cf38eb0a2e366be25db0df59ca4eb00f18674523
stefan-zander <stefan.zander@th-bingen.de>
2018-07-31 14:16:47 +0200

    Added documentation to GermanDateFormatter

--- Weinbergapp/Weinbergapp/HelperClasses/GermanDateFormatter.swift
+++ Weinbergapp/Weinbergapp/HelperClasses/GermanDateFormatter.swift
@@ -8,8 +8,12 @@
 
 import Foundation
 
+/**
+ Formats a date according to german date formats.
+ */
 public class GermanDateFormatter {
 
+    /// Returns the singleton formatting instance
     public static let shared = GermanDateFormatter()
 
     private let dateFormatter: DateFormatter
@@ -21,12 +25,24 @@ public class GermanDateFormatter {
         self.dateFormatter = dateFormatter
     }
 
+    /**
+     Returns a string representation of a given date formatted using the german date format.
+ 
+     - Parameter date: The date to format.
+     - Returns: A string representation of date formatted using the german date format.
+    */
     public func string(from date: Date) -> String {
         return dateFormatter.string(from: date)
     }
 
+    /**
+     Returns a date representation of a given string interpreted using the german date format.
+     
+     - Parameter string: The string to parse.
+     - Returns: A date representation of string interpreted using the german date format. If date(from:) can not parse
+                the string, returns nil.
+     */
     public func date(from string: String) -> Date? {
         return dateFormatter.date(from: string)
     }
-
 }
