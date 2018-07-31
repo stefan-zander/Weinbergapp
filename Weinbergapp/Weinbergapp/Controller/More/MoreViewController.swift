3d8f2d46ca0dd3a071bd9cf709fa9dbc3b94c917
stefan-zander <stefan.zander@th-bingen.de>
2018-07-31 12:40:13 +0200

    Reworked constraints for the view of MoreViewController

--- Weinbergapp/Weinbergapp/Controller/More/MoreViewController.swift
+++ Weinbergapp/Weinbergapp/Controller/More/MoreViewController.swift
@@ -14,8 +14,10 @@ class MoreViewController: UIViewController {
         super.viewDidLoad()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func setupBusiness(_ sender: UIButton) {
     }
+    
+    @IBAction func logout(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }    
 }
