57c5d052a0715762f455434f821cafa51f4f6687
stefan-zander <stefan.zander@th-bingen.de>
2018-07-31 17:09:17 +0200

    Updated sources for Eiffel Tower measurement in tests

--- Weinbergapp/WeinbergappTests/PolygonAreaTests.swift
+++ Weinbergapp/WeinbergappTests/PolygonAreaTests.swift
@@ -76,7 +76,7 @@ class PolygonAreaTests: XCTestCase {
     }
     
     func testComputeAreaEiffleTower() {
-        // given
+        // Source: Coordinates of vertices of the eiffle tower were obtained from MapKit
         let eiffleTower = [
             CLLocationCoordinate2D(latitude: 48.859051240203826, longitude: 2.294493716281107),
             CLLocationCoordinate2D(latitude: 48.858282385487286, longitude: 2.2957060566096268),
@@ -84,13 +84,13 @@ class PolygonAreaTests: XCTestCase {
             CLLocationCoordinate2D(latitude: 48.858248388220119, longitude: 2.2932913134106578)
         ]
         
-        // when
-        let actualAreaInSquareMeters = computeArea(coordinates: eiffleTower)
+        // Source: https://www.wonders-of-the-world.net/Eiffel-Tower/Dimensions-of-the-Eiffel-tower.php
         let widthOfEiffleTowerInMeters = 125.0
         let surfaceOfEiffleTowerInSquareMeters = widthOfEiffleTowerInMeters * widthOfEiffleTowerInMeters
+        let computedAreaInSquareMeters = computeArea(coordinates: eiffleTower)
         
         // then
-        XCTAssertEqual(actualAreaInSquareMeters,
+        XCTAssertEqual(computedAreaInSquareMeters,
                        surfaceOfEiffleTowerInSquareMeters,
                        accuracy: 10.0) // 10 square meter accuracy
     }
