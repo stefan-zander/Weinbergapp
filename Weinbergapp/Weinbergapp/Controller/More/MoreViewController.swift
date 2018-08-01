290340144a17a153f5800950450f8a50c6f9d0e1
stefan-zander <stefan.zander@th-bingen.de>
2018-08-01 10:11:20 +0200

    Added QR code scanner

--- Weinbergapp/Weinbergapp/Controller/More/MoreViewController.swift
+++ Weinbergapp/Weinbergapp/Controller/More/MoreViewController.swift
@@ -7,15 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
+import QRCodeReader
 
 class MoreViewController: UIViewController {
 
+    // Good practice: create the reader lazily to avoid cpu overload during the
+    // initialization and each time we need to scan a QRCode
+    lazy var readerVC: QRCodeReaderViewController = {
+        let builder = QRCodeReaderViewControllerBuilder {
+            $0.reader = QRCodeReader(metadataObjectTypes: [.qr], captureDevicePosition: .back)
+        }
+        
+        return QRCodeReaderViewController(builder: builder)
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func setupBusiness(_ sender: UIButton) {
-        // TODO add QR scanner
+        readerVC.completionBlock = { (result: QRCodeReaderResult?) in
+            print(result)
+        }
+        
+        // Presents the readerVC as modal form sheet
+        readerVC.modalPresentationStyle = .formSheet
+        present(readerVC, animated: true, completion: nil)
     }
 
     @IBAction func logout(_ sender: UIButton) {
